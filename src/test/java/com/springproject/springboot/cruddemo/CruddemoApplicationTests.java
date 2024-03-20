d252b89726ca5c6c2317f9bac52e873c26e95d65
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-20 10:54:56 -0500

    Initial commit

---
+++ src/test/java/com/springproject/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
