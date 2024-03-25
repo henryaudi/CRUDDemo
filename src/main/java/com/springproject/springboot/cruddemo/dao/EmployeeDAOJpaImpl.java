683de868c30e2463909d052fd9af95c4f93395bd
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-25 00:30:42 -0500

    Updated EmployeeDAOJpaImpl.java with findAll() method

--- src/main/java/com/springproject/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/springproject/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -2,6 +2,7 @@ package com.springproject.springboot.cruddemo.dao;
 
 import com.springproject.springboot.cruddemo.entity.Employee;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -21,6 +22,14 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> findAll() {
-        return null;
+
+        // create a query
+        TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+        return employees;
     }
 }
